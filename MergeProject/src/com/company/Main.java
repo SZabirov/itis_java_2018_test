9abd5b60e11e31aaaf892a3b0716e9d4809c00a4
Student <example@example.com>
2018-03-03 17:22:48 +0300

    Второй сканнер добавле

--- MergeProject/src/com/company/Main.java
+++ MergeProject/src/com/company/Main.java
@@ -7,7 +7,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(new FileInputStream("file1.txt"));
-        int x = sc.nextInt();
+        Scanner sc1 = new Scanner(new FileInputStream("file1.txt"));
+        int x = sc1.nextInt();
+        Scanner sc2 = new Scanner(new FileInputStream("file2.txt"));
+        int x2 = sc2.nextInt();
     }
 }
