27041e02d05599c7725ae17652c15530564080f7
Student <example@example.com>
2018-03-03 18:51:45 +0300

    Алгоритм слияния почти готов

--- MergeProject/src/com/company/Main.java
+++ MergeProject/src/com/company/Main.java
@@ -2,14 +2,37 @@ package com.company;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
+/**
+ * Есть два файла с отсортированными последовательностями чисел
+ *
+ */
 public class Main {
-
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc1 = new Scanner(new FileInputStream("file1.txt"));
-        int x = sc1.nextInt();
         Scanner sc2 = new Scanner(new FileInputStream("file2.txt"));
-        int x2 = sc2.nextInt();
+        PrintWriter pw = new PrintWriter(new FileOutputStream("output.txt"));
+        int a1 = sc1.nextInt();
+        int a2 = sc2.nextInt();
+        while (sc1.hasNextInt() & sc2.hasNextInt()) {
+            if (a1 < a2) {
+                pw.println(a1);
+                a1 = sc1.nextInt();
+            } else {
+                pw.println(a2);
+                a2 = sc2.nextInt();
+            }
+        }
+        if (!sc1.hasNextInt()) {
+            pw.println(a1);
+        }
+        if (!sc2.hasNextInt()) {
+            pw.println(a2);
+        }
+        //todo пока в file2 есть числа, выводить их в output
+        pw.close();
     }
 }
